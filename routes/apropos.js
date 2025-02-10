f1ac0092eb23f7fc697db7ba0ee6e395bbb755d5
Espace NUMERIQUE 9 <espace9@numerique.lan>
2025-02-10 15:42:27 +0300

    mon travail

---
+++ routes/apropos.js
@@ -0,0 +1,17 @@
+// Importation du module 'express' qui permet de créer une application web avec Express.js
+const express = require('express');
+
+///méthode qui gère la vue de la page 'apropos
+const aproposcontrollers = require('../controllers/apropos'); 
+
+// Création d'une instance du routeur d'Express pour gérer les routes
+const router = express.Router();
+
+// Définition d'une route GET pour la page d'accueil (ou la page apropos dans ce cas)
+// Lorsque l'utilisateur accède à la route '/', la méthode 'aproposView' du contrôleur 'aproposcontroller' sera appelée
+
+
+router.get('/', aproposcontrollers.aproposViews);
+
+
+module.exports = router;
